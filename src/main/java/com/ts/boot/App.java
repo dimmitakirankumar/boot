582fc1525529cceacf837179fd16ac0d6a661ce1
dimmitakirankumar <89284982+dimmitakirankumar@users.noreply.github.com>
2021-11-21 02:28:40 +0530

    Update App.java

--- src/main/java/com/ts/boot/App.java
+++ src/main/java/com/ts/boot/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello kiran this is pav" );
+        System.out.println( "Hello kiran this is mahesh " );
     }
 }
